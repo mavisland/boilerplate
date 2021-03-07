36eae5c606800b0430f4b838a17d25c6ccac5842
Tanju Yildiz <ben@tanjuyildiz.com>
2021-03-07 22:19:13 +0300

    Added templates task
    Signed-off-by: Tanju Yildiz <ben@tanjuyildiz.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,44 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+"use strict";
 
-exports.default = defaultTask;
+// Packages
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const fs = require("fs");
+const data = require("gulp-data");
+const twig = require("gulp-twig");
+
+// Paths
+const paths = {
+  html: {
+    input: "src/html/*.twig",
+    output: "dist/",
+  },
+};
+
+gulp.task(
+  "templates",
+  gulp.series(function (cb) {
+    gulp
+      .src(paths.html.input)
+      .pipe(
+        plumber({
+          handleError: function (err) {
+            console.log(err);
+            this.emit("end");
+          },
+        })
+      )
+      .pipe(
+        data(function (file) {
+          return JSON.parse(fs.readFileSync("boilerplate.json"));
+        })
+      )
+      .pipe(twig())
+      .pipe(gulp.dest(paths.html.output));
+
+    // Callback
+    cb();
+  })
+);
+
+gulp.task("default", gulp.parallel(["templates"]));
